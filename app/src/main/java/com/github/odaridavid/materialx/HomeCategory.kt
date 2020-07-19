c8f84aff3c5f34f42450d4bf8a9bed5bd0466e63
Davidodari <davidkibzodari@gmail.com>
2020-07-19 16:51:18 +0300

    add motion layout sample

--- app/src/main/java/com/github/odaridavid/materialx/HomeCategory.kt
+++ app/src/main/java/com/github/odaridavid/materialx/HomeCategory.kt
@@ -61,6 +61,11 @@ internal fun getHomeCategories(): List<HomeCategory> {
             R.string.label_grids,
             R.drawable.ic_grid_24,
             R.id.action_homeFragment_to_gridsFragment
+        ),
+        HomeCategory(
+            R.string.label_motion,
+            R.drawable.ic_motion_24,
+            R.id.action_homeFragment_to_motionLayoutFragment
         )
     )
 }
